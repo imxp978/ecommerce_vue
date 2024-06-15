98878aaf24d814870df161be952e144f8c3cb241
chun <quelquefoi@gmail.com>
2024-06-15 20:12:30 +0800

    rebuild

---
+++ dist/assets/ProductsView-BIUzBX53.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./Products-OevO2r8-.js";import{c as r,o as t}from"./index-D772MwYD.js";import"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js";import"https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js";const i={__name:"ProductsView",setup(c){return(e,_)=>(t(),r(o))}};export{i as default};
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import router from './router'
+import router from './router/index.js'
 
 import 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js'
 import 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js'
